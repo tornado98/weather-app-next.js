79cd1c4d36a10fa04c0f08f577f375df368b3d7f
tornado98 <taherim36@gmail.com>
2023-02-05 23:50:44 +0330

    m.reza: first init

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import spinner from '../../public/spinner.gif';
+
+const Spinner = () => {
+  return (
+    <>
+
+        <Image className='w-[200px] m-auto block' src={spinner} alt='loading...'/>
+    </>
+  )
+}
+
+export default Spinner
